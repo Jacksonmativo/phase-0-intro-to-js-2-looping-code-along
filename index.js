2b74b330f81ca8962399e5b54297431a5afe0fee
jackson mativo <jacksonmativo21@gmail.com>
2025-03-19 19:53:48 -0400

    master

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []; // Create an empty array to store messages
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return messages;
+  }
+  
+  // Test the function
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Test the function
+  countDown(10);
+  
